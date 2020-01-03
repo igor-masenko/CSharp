cdbc7e2d83ccaa1420a51763a831a37c57d30090
Igor Masenko <masenko_igor@icloud.com>
2020-01-03 17:12:52 +0300

    Project One: Wake Up, Neo - Console's msg

--- LearningCSharp/1. Wake Up, Neo/WakeUpNeo/MainProgram.cs
+++ LearningCSharp/1. Wake Up, Neo/WakeUpNeo/MainProgram.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace WakeUpNeo
 {
@@ -10,6 +7,24 @@ namespace WakeUpNeo
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green; // text color
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight); // console size
+            Console.SetWindowPosition(0, 0); // console position
+
+            Console.WriteLine("Wake up, Neo...");
+            Thread.Sleep(3000); // 3 seconds
+            
+            Console.Clear();
+            Console.WriteLine("The matrix has you...");
+            Thread.Sleep(3000);
+
+            Console.Clear();
+            Console.WriteLine("Follow of the white rabbit.");
+            Thread.Sleep(3000);
+
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo...");
+            Thread.Sleep(3000);
         }
     }
 }
