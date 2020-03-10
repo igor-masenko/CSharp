e042132d924e8a6d6b2f8c69f27443734d5e732e
Igor Masenko <masenko_igor@icloud.com>
2020-03-10 22:15:54 +0300

    Algorithm.Sort: Добавлена сортировка слиянием (asc)
    Добавлена функция слияния (merge) и сортировка MergeSortAsc

--- Algorithm/Sort/Program.cs
+++ Algorithm/Sort/Program.cs
@@ -8,6 +8,15 @@ namespace Sort
 {
     class Program
     {
+
+        static private void PrintArray(List<int> array, string text = "")
+        {
+            Console.Write(text);
+            foreach (var element in array)
+                Console.Write($"{element} ");
+            Console.WriteLine();
+        }
+
         static private void InsertionSortAsc(List<int> array)
         {
             for (var sortedIndex = 1; sortedIndex < array.Count; sortedIndex++)
@@ -35,24 +44,76 @@ namespace Sort
                 }
             }
         }
+
+        static private void MergeAsc(List<int> array, int beginingIndex, int separatorIndex, int endingIndex)
+        {
+            if (beginingIndex <= endingIndex 
+                && ((beginingIndex <= separatorIndex) || (separatorIndex <= endingIndex)))
+            {
+                var leftArrayLength = separatorIndex - beginingIndex + 1;
+                var rightArrayLength = endingIndex - separatorIndex;
+
+                var leftArray = new List<int>(array.GetRange(beginingIndex, leftArrayLength));
+                var rightArray = new List<int>(array.GetRange(separatorIndex + 1, rightArrayLength));
+
+                var leftArrayIndex = 0;
+                var rightArrayIndex = 0;
+
+                for (int arrayIndex = beginingIndex; arrayIndex <= endingIndex; arrayIndex++)
+                {
+                    if (rightArrayIndex < rightArrayLength && leftArrayIndex < leftArrayLength)
+                    {
+                        if (leftArray[leftArrayIndex] <= rightArray[rightArrayIndex])
+                        {
+                            array[arrayIndex] = leftArray[leftArrayIndex];
+                            leftArrayIndex++;
+                        }
+                        else
+                        {
+                            array[arrayIndex] = rightArray[rightArrayIndex];
+                            rightArrayIndex++;
+                        }
+                    }
+                    else if (leftArrayLength >= leftArrayIndex && rightArrayIndex < rightArrayLength)
+                    {
+                        array[arrayIndex] = rightArray[rightArrayIndex];
+                        rightArrayIndex++;
+                    }
+                    else if (rightArrayLength >= rightArrayIndex && leftArrayIndex < leftArrayLength)
+                    {
+                        array[arrayIndex] = leftArray[leftArrayIndex];
+                        leftArrayIndex++;
+                    }
+                }
+            }
+        }
+
+        static private void MergeSortAsc(List<int> array, int beginingIndex, int endingIndex)
+        {
+            if (beginingIndex < endingIndex)
+            {
+                var separatorIndex = (int)Math.Floor((decimal)(beginingIndex + endingIndex) / 2);
+                MergeSortAsc(array, beginingIndex, separatorIndex);
+                MergeSortAsc(array , separatorIndex + 1, endingIndex);
+                MergeAsc(array, beginingIndex, separatorIndex, endingIndex);
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            List<int> array = new List<int> { 1, 5, 2, 6, 8, 1};
+            List<int> array = new List<int> { 1, 5, 2, 6, 8, 1, 10 };
             InsertionSortAsc(array);
-
-            foreach(var element in array)
-            {
-                Console.Write("{0} ", element);
-            }
-            Console.WriteLine();
+            PrintArray(array, "Insertion sort asc: \t");
 
             InsertionSortDesc(array);
+            PrintArray(array, "Insertion sort desc: \t");
 
-            foreach (var element in array)
-            {
-                Console.Write("{0} ", element);
-            }
-            Console.WriteLine();
+            MergeSortAsc(array, 0, array.Count - 1);
+            PrintArray(array, "Merge sort asc: \t");
+
+            Console.ReadKey();
+            
         }
 
     }
